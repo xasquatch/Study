9a4b548bac6e4b87bad1bf9d8ba219123faba667
TrueZ <yume7168@naver.com>
2019-04-19 12:49:47 +0900

    0419 JAVA

---
+++ workspace_Java/JavaBook/src/Chapter12/RegexpTest.java
@@ -0,0 +1,23 @@
+package Chapter12;
+
+import java.text.MessageFormat;
+import java.util.regex.Pattern;
+
+public class RegexpTest {
+
+	public static void main(String[] args) {
+
+		String regexp = "^(010|011)[-\\s]?\\d{3,4}[-\\s]?\\d{4}";
+		String[] nums = {
+				"010-000-1234",
+				"01112345678",
+				"010)1234 5678",
+				"010 1234 567a"};
+		String resultPattern = "{0}는 전화번호 타입인가? {1}";
+		for (String num : nums) {
+			boolean result = Pattern.matches(regexp, num);
+			System.out.println(MessageFormat.format(resultPattern, num, result));
+		}
+	}
+
+}
