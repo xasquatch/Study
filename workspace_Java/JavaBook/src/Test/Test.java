52fe62a4e318c776f3b84bcf1f88b4c363aa0bb5
TrueZ <yume7168@naver.com>
2019-05-09 12:47:00 +0900

    0509 JAVA next . ch16

---
+++ workspace_Java/JavaBook/src/Test/Test.java
@@ -0,0 +1,27 @@
+package Test;
+
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+class Test1 extends JFrame{
+
+	public Test1() {
+		setTitle("Thread Test");
+		setSize(600,600);
+		setVisible(true);
+	}
+	
+	
+}
+
+public class Test {
+
+	public static void main(String[] args) {
+		Test1 t1 = new Test1();
+		
+		System.out.println("메인 끝남");
+		
+	}
+
+}
