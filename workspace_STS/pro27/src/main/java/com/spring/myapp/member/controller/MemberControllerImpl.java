ce9237e95fa3fcaa67b731a5d643752da5bb0fd7
TrueZ <yume7168@naver.com>
2019-08-05 17:42:37 +0900

    0805 STS 스프링 레거시 MVC프로젝트 메이븐 설정

---
+++ workspace_STS/pro27/src/main/java/com/spring/myapp/member/controller/MemberControllerImpl.java
@@ -0,0 +1,104 @@
+package com.spring.myapp.member.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.myapp.member.service.MemberService;
+import com.spring.myapp.member.vo.MemberVO;
+
+//@Controller애너테이션기호를 이용해  MemberControllerImpl클래스에 대해..
+//id가 memberController인 빈을 자동 생성합니다.
+@Controller("memberController")
+public class MemberControllerImpl   implements MemberController {
+	//Autowired애너테이션 기호를 이용해 setter메소드를 사용하지 않고
+	//id가 memberService인  빈을 자동 주입합니다.
+	@Autowired
+	private MemberService memberService;
+	@Autowired
+	MemberVO memberVO ;
+	
+	@Override
+	@RequestMapping(value="/member/listMembers.do" ,method = RequestMethod.GET)
+	public ModelAndView listMembers(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String viewName = getViewName(request);
+		List membersList = memberService.listMembers();
+		ModelAndView mav = new ModelAndView(viewName);
+		mav.addObject("membersList", membersList);
+		return mav;
+	}
+
+	@Override
+	@RequestMapping(value="/member/addMember.do" ,method = RequestMethod.POST)
+	public ModelAndView addMember(@ModelAttribute("member") MemberVO member,
+			                  HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setCharacterEncoding("utf-8");
+		int result = 0;
+		result = memberService.addMember(member);
+		ModelAndView mav = new ModelAndView("redirect:/member/listMembers.do");
+		return mav;
+	}
+	
+	@Override
+	@RequestMapping(value="/member/removeMember.do" ,method = RequestMethod.GET)
+	public ModelAndView removeMember(@RequestParam("id") String id, 
+			           HttpServletRequest request, HttpServletResponse response) throws Exception{
+		request.setCharacterEncoding("utf-8");
+		memberService.removeMember(id);
+		ModelAndView mav = new ModelAndView("redirect:/member/listMembers.do");
+		return mav;
+	}
+	
+	/*@RequestMapping(value = { "/member/loginForm.do", "/member/memberForm.do" }, method =  RequestMethod.GET)*/
+	@RequestMapping(value = "/member/*Form.do", method =  RequestMethod.GET)
+	public ModelAndView form(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String viewName = getViewName(request);
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName(viewName);
+		return mav;
+	}
+	
+	
+
+	private String getViewName(HttpServletRequest request) throws Exception {
+		String contextPath = request.getContextPath();
+		String uri = (String) request.getAttribute("javax.servlet.include.request_uri");
+		if (uri == null || uri.trim().equals("")) {
+			uri = request.getRequestURI();
+		}
+
+		int begin = 0;
+		if (!((contextPath == null) || ("".equals(contextPath)))) {
+			begin = contextPath.length();
+		}
+
+		int end;
+		if (uri.indexOf(";") != -1) {
+			end = uri.indexOf(";");
+		} else if (uri.indexOf("?") != -1) {
+			end = uri.indexOf("?");
+		} else {
+			end = uri.length();
+		}
+
+		String viewName = uri.substring(begin, end);
+		if (viewName.indexOf(".") != -1) {
+			viewName = viewName.substring(0, viewName.lastIndexOf("."));
+		}
+		if (viewName.lastIndexOf("/") != -1) {
+			viewName = viewName.substring(viewName.lastIndexOf("/"), viewName.length());
+		}
+		return viewName;
+	}
+
+
+}
---
+++ workspace_STS/pro27/src/main/java/com/spring/myapp/member/dao/MemberDAO.java
@@ -0,0 +1,16 @@
+package com.spring.myapp.member.dao;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+
+import com.spring.myapp.member.vo.MemberVO;
+
+
+public interface MemberDAO {
+	 public List selectAllMemberList() throws DataAccessException;
+	 public int insertMember(MemberVO memberVO) throws DataAccessException ;
+	 public int deleteMember(String id) throws DataAccessException;
+	 
+
+}
