5220f226af9aadbd72be809f4320587f207971b3
Bigfoot <yume7168@naver.com>
2019-08-20 22:23:05 +0900

    레파지토리정리:팀프로젝트 회의내용 추가

---
+++ Study-TeamProject-base-master/desc table list.sql
@@ -0,0 +1,19 @@
+show databases;
+SET FOREIGN_KEY_CHECKS = 1;
+
+use learnrun;
+show tables;
+
+desc bank;
+desc bank_name;
+desc buyer;
+desc category;
+desc class;
+desc coupon;
+desc curriculm;
+desc jjim;
+desc message;
+desc receiver_info;
+desc reply;
+desc user;
+desc user_group;
---
+++ Study-TeamProject-base-master/모델링/learn_run.sql
@@ -0,0 +1,23 @@
+show databases;
+
+use learnrun;
+
+show tables;
+SET foreign_key_checks = 0;
+SET foreign_key_checks = 1;
+drop table learnrun.rank;
+
+class
+class_user
+coupon
+curriculum
+delivery_info
+faq
+image
+item
+message
+order_info
+price
+rank
+user
+user_has_coupon
